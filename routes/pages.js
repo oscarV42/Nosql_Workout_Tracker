239800c8546b39c61617f7c6a44c2a50ba392f52
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-10-28 22:51:54 -0400

    updating page routes and fixing file paths to render the correct html page

--- routes/pages.js
+++ routes/pages.js
@@ -1,15 +1,16 @@
 const router = require("express").Router();
+const path = require('path')
 
 router.get('/', (req, res) => {
-    res.sendFile(__dirname + '../public/index.html')
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 router.get('/stats', (req, res) => {
-    res.sendFile(__dirname + '../public/stats.html')
+    res.sendFile(path.join(__dirname, '../public/stats.html'))
 })
 
 router.get('/exercise', (req, res) => {
-    res.sendFile(__dirname + '../public/exercise.html')
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
 })
 
 module.exports = router
