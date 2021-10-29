ce29828af540d8903155e0f67a99bae8c1fe35bc
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-10-28 23:22:32 -0400

    creating POST route for creating a new workout within the database

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-
+router.post('/workouts', async ({ body }, res) => {
+    try{
+        const dbWorkout = await Workout.create(body);
+        if (dbWorkout) {
+            res.status(201).send(dbWorkout);
+        } else {
+          res.status(400).send('workout not created');
+        }
+    } catch (err){
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router
