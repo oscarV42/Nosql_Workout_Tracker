f33470a76919e5b455461310f832975632f66692
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-10-29 21:32:46 -0400

    creating GET route for finding all workouts in the database

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,25 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+router.get('/workouts', async ({ body }, res) => {
+    try{
+        const dbWorkouts = await Workout.find({})
+        .sort({ day: 1 })
+        if(dbWorkouts){
+            res.status(200).json(dbWorkouts)
+        }else{
+            res.status(400).json({ message: 'No workouts found in the database!'})
+        }
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
 router.post('/workouts', async ({ body }, res) => {
     try{
         const dbWorkout = await Workout.create(body);
         if (dbWorkout) {
-            res.status(201).send(dbWorkout);
+            res.status(200).send(dbWorkout);
         } else {
           res.status(400).send('workout not created');
         }
