1dd4a0170db3d39b514445831da6671b7a6abd19
Oscar Vizcaino <oscar.a.vizcaino@icloud.com>
2021-10-29 23:05:01 -0400

    creating PUT route for updating a workout by its id within the database

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 router.get('/workouts', async ({ body }, res) => {
-    try{
+    try {
         const dbWorkouts = await Workout.find({})
         .sort({ day: 1 })
         if(dbWorkouts){
@@ -10,21 +10,41 @@ router.get('/workouts', async ({ body }, res) => {
         }else{
             res.status(400).json({ message: 'No workouts found in the database!'})
         }
-    }catch(err){
+    }catch(err) {
         res.status(500).json(err)
     }
-})
+});
 
 router.post('/workouts', async ({ body }, res) => {
-    try{
+    try {
         const dbWorkout = await Workout.create(body);
         if (dbWorkout) {
             res.status(200).send(dbWorkout);
         } else {
           res.status(400).send('workout not created');
         }
-    } catch (err){
+    } catch (err) {
         res.status(500).json(err);
+    }
+});
+
+router.put('/workouts/:id', async ({ params }, res) => {
+    try {
+        const dbWorkout = await Workout.updateOne({
+            _id: mongojs.ObjectId(params.id)
+        },
+        {
+            $set: {
+                exercise: params
+            }
+        })
+        if(dbWorkout){
+            res.status(200).json(dbWorkout)
+        }else {
+            res.status(400).json({ message: 'No workout found under this id!' })
+        }
+    }catch(err) {
+        res.status(500).json(err)
     }
 })
 
